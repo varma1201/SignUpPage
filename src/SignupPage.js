9031b6c033aa26877d752c8a89dd55a26bcfa1ed
varma1201 <vikramvarma1201@gmail.com>
2023-06-29 10:33:08 +0530

    first commit

--- src/SignupPage.js
+++ src/SignupPage.js
@@ -1,6 +1,22 @@
+import { useState, useEffect } from 'react';
 import './signupPage.css';
 const Signup = () => {
-  const submitHandler = () => {};
+  const [username, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+  const [Phone, setPhone] = useState('');
+  const [email, setEmail] = useState('');
+  const [data, setData] = useState('');
+  const url = 'https://surveyor-app-server.vercel.app/users/';
+  const UserDetails = {};
+  const submitHandler = (e) => {
+    e.preventDefault();
+    UserDetails.Username = username;
+    UserDetails.password = password;
+    UserDetails.Phone = Phone;
+    UserDetails.email = email;
+    console.log(UserDetails);
+  };
+
   return (
     <>
       <div className='container'>
@@ -8,21 +24,58 @@ const Signup = () => {
           <form>
             <div className='tag'>
               <div>User Name:</div>
-              <input type='text' name='' id='' className='field' />
+              <input
+                type='text'
+                name=''
+                id=''
+                className='field'
+                onChange={(e) => {
+                  setUserName(e.target.value);
+                }}
+              />
             </div>
             <div className='tag'>
               <div>Password:</div>
-              <input type='text' name='' id='' className='field' />
+              <input
+                type='text'
+                name=''
+                id=''
+                className='field'
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+              />
             </div>
             <div className='tag'>
               <div>Phone Number:</div>
-              <input type='text' name='' id='' className='field' />
+              <input
+                type='text'
+                name=''
+                id=''
+                className='field'
+                onChange={(e) => {
+                  setPhone(e.target.value);
+                }}
+              />
             </div>
             <div className='tag'>
               <div>Email:</div>
-              <input type='text' name='' id='' className='field' />
+              <input
+                type='text'
+                name=''
+                id=''
+                className='field'
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+              />
             </div>
-            <button type='submit' onClick={submitHandler}>
+            <button
+              type='submit'
+              onClick={(e) => {
+                submitHandler(e);
+              }}
+            >
               Submit
             </button>
           </form>
